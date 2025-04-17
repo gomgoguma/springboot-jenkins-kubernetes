29a8cd7d4607cac5326640036a701a37f0dfbf24
gomgoguma <kebhse@daum.net>
2025-04-18 00:34:08 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/springbootjenkinskubernetes/SpringbootJenkinsKubernetesApplication.java
@@ -0,0 +1,20 @@
+package com.example.springbootjenkinskubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootJenkinsKubernetesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJenkinsKubernetesApplication.class, args);
+    }
+
+    @GetMapping("")
+    public String hello() {
+        return "Hello World";
+    }
+}
